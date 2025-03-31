abd3661b4507ce5812bc747a60556d3144440065
vlacheslav katerynych <bvelorant@gmail.com>
2025-03-31 14:35:12 +0300

    back time

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
        }
 
        function formatPlaytime(minutes) {
-           if (minutes === 0) {
-               return "Час невизначений";
-           }
+           
+           
            const hours = Math.floor(minutes / 60);
            const remainingMinutes = minutes % 60;
            return `${hours}h ${remainingMinutes}m`;
